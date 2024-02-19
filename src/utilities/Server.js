5d10241408d2993a1c66ead2fbf29713d392e3b6
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2024-02-19 16:50:20 +0100

    url

--- src/utilities/Server.js
+++ src/utilities/Server.js
@@ -4,7 +4,7 @@ const API_BASE_URL = "https://plomberie-serveur.onrender.com/api";
 export const loginUser = async (email, password) => {
     try {
         // On envoie une requête de connexion POST à l'API de connexion avec les données de l'utilisateur saisies dans le formulaire
-        const response = await fetch(`${API_BASE_URL}/login`, {
+        const response = await fetch(`https://plomberie-serveur.onrender.com/api/auth/login`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
