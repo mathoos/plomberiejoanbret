98613ad4b413edb0886af9d6d8c06a85379fbd9a
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2024-02-25 17:13:48 +0100

    compression images

--- src/pages/Realisations.js
+++ src/pages/Realisations.js
@@ -1,7 +1,7 @@
 import Header from '../components/Header';
 import Galerie from '../components/Galerie';
 import Footer from '../components/Footer';
-import HeaderImg from "../img/galerie/evier-eau.jpg";
+import HeaderImg from "../img/galerie/evier-eau-compressed.jpg";
 
 
 function Home() {
