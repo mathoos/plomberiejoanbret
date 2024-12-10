0330f5446ac4ffd8d88c90862c191a700a904b1e
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2024-12-10 13:13:27 +0100

    slider & lightbox

--- src/components/Realisations.js
+++ src/components/Realisations.js
@@ -64,7 +64,7 @@ const Realisations = () => {
                 slidesRef.current.style.transform = `translateX(-${currentImageIndex * 100}vw)`; // Ajuster pour mobile
             } 
             else {
-                slidesRef.current.style.transform = `translateX(-${currentImageIndex * 40}vw)`; // Valeur par défaut pour écran plus large
+                slidesRef.current.style.transform = `translateX(-${currentImageIndex * 35}vw)`; // Valeur par défaut pour écran plus large
             }
         }
     }, [currentImageIndex, isMobile]);
@@ -152,7 +152,7 @@ const Realisations = () => {
                 slidesRef.current.style.transform = `translateX(-${currentImageIndex * 100}vw)`; // Par exemple, plus large pour mobile
             } else {
                 // Sinon, pour les écrans plus larges, on applique la transformation par défaut
-                slidesRef.current.style.transform = `translateX(-${currentImageIndex * 40}vw)`;
+                slidesRef.current.style.transform = `translateX(-${currentImageIndex * 35}vw)`;
             }
             slidesTitlesRef.current.style.transition = `transform 1s ease-in-out`;
             slidesTitlesRef.current.style.transform = `translateY(-${currentTitleIndex * maxHeightTitles}px)`;
